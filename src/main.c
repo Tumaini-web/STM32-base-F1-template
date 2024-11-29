7a1469eb7aed3ebf2e5174d046133ac444d51b43
Tumaini Manyama <tuma.manyama7@gmail.com>
2024-11-29 22:48:26 +0700

    added changes to make it work on my machine

--- src/main.c
+++ src/main.c
@@ -7,24 +7,35 @@ static void delay (unsigned int time) {
 }
 
 int main (void) {
-    // Turn on the GPIOC peripheral
+    // Turn on the GPIOC and B peripheral
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 
     // Put pin 13 in general purpose push-pull mode
     GPIOC->CRH &= ~(GPIO_CRH_CNF13);
     // Set the output mode to max. 2MHz
     GPIOC->CRH |= GPIO_CRH_MODE13_1;
 
+    // Put pin 12 in general purpose push-pull mode
+    GPIOB->CRH &= ~(GPIO_CRH_CNF12);
+    // Set the output mode to max. 2MHz
+    GPIOB->CRH |= GPIO_CRH_MODE12_1;
+
     while (1) {
         // Reset the state of pin 13 to output low
         GPIOC->BSRR = GPIO_BSRR_BR13;
 
-        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR12;
+
+        delay(200);
 
         // Set the state of pin 13 to output high
         GPIOC->BSRR = GPIO_BSRR_BS13;
 
-        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BS12;
+
+        delay(200);
     }
 
     // Return 0 to satisfy compiler
