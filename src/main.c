13aaa30f7c7e082446f4de98151983dc5cdef380
Tumaini Manyama <tuma.manyama7@gmail.com>
2025-03-29 16:24:26 +0700

    why no work

--- src/main.c
+++ src/main.c
@@ -13,43 +13,51 @@ int main (void) {
     // Turn on the GPIOC and set it as alternate function
     RCC-> AHBENR |= RCC_AHBENR_GPIOAEN;
 
+    GPIOA->AFR[1]|=(6<<0);
+
     GPIOA -> MODER &= ~(GPIO_MODER_MODER8_0);
     GPIOA -> MODER |= GPIO_MODER_MODER8_1;
 
-    GPIOA -> OTYPER |= GPIO_OTYPER_OT_8;
+    // GPIOA -> OTYPER &= ~(GPIO_OTYPER_OT_8);
 
-    GPIOA -> OSPEEDR &= ~(GPIO_OSPEEDER_OSPEEDR8);
+    // GPIOA -> OSPEEDR |= GPIO_OSPEEDER_OSPEEDR8_0;
 
-    GPIOA -> AFR[7] |= 0X6;
+    // GPIOA -> PUPDR |= GPIO_PUPDR_PUPDR8_0;
 
+    
+    
 
     RCC -> APB2ENR |= RCC_APB2ENR_TIM1EN;
 
-    TIM1->PSC=7999; //set prescaller to 0 (no divider)
-    TIM1->ARR=255; //set the maximum count value
-     //seset the current count
+ 
 
-    TIM1 -> CCMR1 |= TIM_CCMR1_OC1M_1;
-    TIM1 -> CCMR1 |= TIM_CCMR1_OC1M_2;
-    TIM1 -> CCMR1 &= ~(TIM_CCMR1_OC1M_0);
-    TIM1 -> CCMR1 &= ~(TIM_CCMR1_OC1M_3);
+    TIM1->PSC=0; //set prescaller to 0 (no divider)
+    TIM1->ARR=500; //set the maximum count value
+    TIM1->CNT=0; //seset the current count
 
-    TIM1 -> CCMR1 |= TIM_CCMR1_OC1PE;
+    TIM1 -> CCMR1 |= TIM_CCMR1_OC1M_0 | TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;
+    
+    // TIM1 -> CCMR1 &= ~(TIM_CCMR1_OC1M_0);
+    // TIM1 -> CCMR1 &= ~(TIM_CCMR1_OC1M_3);
 
-    TIM1 -> CCER |= TIM_CCER_CC1P;
+    //TIM1 -> EGR |= TIM_EGR_UG;
+
+    //TIM1 -> CCMR1 |= TIM_CCMR1_OC1PE;
+
+    //TIM1 -> CCER &= ~(TIM_CCER_CC1P);
     TIM1 -> CCER |= TIM_CCER_CC1E;
 
 
     TIM1 -> CR1 |= TIM_CR1_CEN;
 
-
+    TIM1 -> CCR1 = 25;
 
 
 
     while (1) {
 
 
-		TIM1 -> CCR1 = 20;
+		
     }
 
     // Return 0 to satisfy compiler
